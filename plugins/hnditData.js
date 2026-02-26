48f5612ff0f2d82a82e8bcc9933f535945febc0b
Shamal Sathsara <shamalsathsara4@gmail.com>
2026-02-26 19:34:44 +0530

    HNDIT Data Enter

--- plugins/hnditData.js
+++ plugins/hnditData.js
@@ -8,15 +8,24 @@ const hnditData = {
                 shortNotes: "LINK_HERE"
             },
             "(1022) Web Design": {
-                pastPapers: "LINK_HERE",
-                markingScheme: "LINK_HERE",
+                pastPapers: "https://drive.google.com/drive/folders/1pG_MgSc7QAvNcNZl5kGOZCXBhzVWx1YP?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1GiM_1ySjAb2fJA6kBhiSJLUwHkCMmQ01?usp=drive_link",
                 shortNotes: "LINK_HERE"
             },
             "(1032) Computer & Network Systems":{
-                pastPapers: "LINK_HERE",
-                markingScheme: "LINK_HERE",
+                pastPapers: "https://drive.google.com/drive/folders/1LVwaHzUYbg0VyfNWXbBGGA9KS4V477TL?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1tRwGSGfNicAXW-j3fNpje_6tSs40ClD6?usp=drive_link",
                 shortNotes: "LINK_HERE"
-
+            },
+            "(1042) Information Management Systems":{
+                pastPapers: "https://drive.google.com/drive/folders/1b0ud0X0FVsXKeOMvt8tgPF7XZ1yMGbA3?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1xIhUqeRAHDS3EB5YjgvVmrZQqbgSQLMJ?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(1062) Communications Skills":{
+                pastPapers: "https://drive.google.com/drive/folders/131QUs465I5TCAyX6iKQUgmnjzpirZPYl?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1GI7aoGzG846TLpC5P8djvOqUhcwGvCOI?usp=drive_link",
+                shortNotes: "LINK_HERE"
             }
 
 
@@ -25,12 +34,46 @@ const hnditData = {
 
         "2nd Semester": {
             "(2012) Fundamentals of Programming": {
-                pastPapers: "LINK_HERE",
-                markingScheme: "LINK_HERE",
+                pastPapers: "https://drive.google.com/drive/folders/18lqy--JmzArMkJQrNjT678GsDzenOm1b?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1g_61TzZUROiUzwHFQmdDnI2XrPatndk7?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(2022) Software Development":{
+                pastPapers: "https://drive.google.com/drive/folders/13Rv1xWzZgPwxLcsO0EoM7sk49ySZaCNk?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1xFCOU9jGbcUeXCS9JYkzDpPVQor1w3Av?usp=drive_link",
+                shortNotes: "LINK_HERE"
+
+            },
+            "(2032) System Analysis & Design":{
+                pastPapers: "https://drive.google.com/drive/folders/1E7b3qUaLkbSMoRC1bbZTnEiQ8STOxWnI?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1-KoSvzr-PcQfQqcNqjU6_KrMbBQ1szuS?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(2042) Data Communication & Networks":{
+                pastPapers: "https://drive.google.com/drive/folders/1aYOcN_nZFbP0hWzSZKLSBuGN4j3F1d1A?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1--KBP_aRYF97NhNexdGHn0PQYLHQC-0n?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(2052) User Interface Design":{
+                pastPapers: "https://drive.google.com/drive/folders/1tHPcPaiYAEzuVPs8oqQ_RHCSmhoA9JDw?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1uoOhJysCIoYzrKdv7ZiiG3koZ97eM3gZ?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(2072) Technical Writing":{
+                pastPapers: "https://drive.google.com/drive/folders/1fxmuz9TQ7b98sR2BfFHkW-bmJE-oc0_I?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1OMI5j5SGbv-q4D5sroIDvrE4vffW3VD-?usp=drive_link",
                 shortNotes: "LINK_HERE"
             }
+
         }
     },
+
+
+
+
+
+
+
 
 
 //Second Year
@@ -38,17 +81,96 @@ const hnditData = {
 
     "2nd Year": {
         "1st Semester": {
-            "(3042) Data Structures and Algorithms": {
-                pastPapers: "LINK_HERE",
-                markingScheme: "LINK_HERE",
+            "(3012) Object Oriented Programming-(OOP)": {
+                pastPapers: "https://drive.google.com/drive/folders/1eBVIPbnG6KDpoQB2laCDPzOt4J1C7BKp?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/186AhNTILssFxmqKtb2GEphxPEU85kgg6?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(3022) Web Programming":{
+                pastPapers: "https://drive.google.com/drive/folders/1phwwiaY-ZF6sehZHEraCLDP-ie40r6j2?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1JqajeSPxh-SO_kAjiFh0l-OZIVXNDxbp?usp=drive_link",
+                shortNotes: "LINK_HERE"
+
+            },
+            "(3032) Data Structures and Algorithms":{
+                pastPapers: "https://drive.google.com/drive/folders/10yB5OEpF0f2yvKmlU9W3FI033DT3BYN2?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1zx17SL_2yshFrcmHxrS5_kJgwO4E3kng?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(3042) Database Management Systems":{
+                pastPapers: "https://drive.google.com/drive/folders/1xbnOsEiVOINW3RMGpNnduy7RCeH-UerC?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1sPmZsIgR-J83Q2kX_j5IgmLyMNJZ98SM?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(3052) Operating Systems":{
+                pastPapers: "https://drive.google.com/drive/folders/1Gc25ads5Aga1jMkBphPZi7YRIHxKVQnO?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/10XCBZT4frcz-YDgop-DVvo86BvNfI0MU?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(3062) Information and Computer Security":{
+                pastPapers: "https://drive.google.com/drive/folders/1WcSPapXu_Cbnq5XZ1VLcLv3sydh56Dgs?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1rweXWTi9Qvqp-IAyypxEJSTqI4mOJztd?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(3072) Statistics for IT":{
+                pastPapers: "https://drive.google.com/drive/folders/1xpn6Nex3gY3ofVKsvxy5n_Vw0qj_iBuP?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1WBtcIaImEv3u2PUcvQItJKAfGI8vo5nn?usp=drive_link",
                 shortNotes: "LINK_HERE"
             }
+
         },
+
+
+
+
+
+
+
+
+
+
+
+        //Second sem
         "2nd Semester": {
+            "(4012) Software Engineering":{
+                pastPapers: "https://drive.google.com/drive/folders/19I6VzqMygnczBA5Ll-5EHZQQVjsyTwqO?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1xVlqCDp2gL97qzTYSLkWztfmwOX6t3LJ?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
             "(4022) Software Quality Assurance": {
-                pastPapers: "LINK_HERE",
+                pastPapers: "https://drive.google.com/drive/folders/1LMDRLntKjgll-HenPWvPCzXZfc_jC0Ua?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1Eymke8cMbcLrLVD7KFuYjZcCKKEBlr-V?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(4032) IT Project Management":{
+                pastPapers: "https://drive.google.com/drive/folders/1JHbLSguuizu0XjM3e7aPo00uuij0f0M4?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1fHJW4XWBYy74jTIAtYpC5iOQrGAOjl47?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(4042) Professional World":{
+                pastPapers: "https://drive.google.com/drive/folders/138M79qm5HeWx18uGmNtge4K0DJuPHEAm?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1cWqqmVt7JYQji5DTTGoTjaAITbqUSdE2?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(4212) Machine Learning":{
+                pastPapers: "https://drive.google.com/drive/folders/1KG2exxNx1d_QRR1FpDmk7ghwg_MtQ5ZF?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1rihZ3U00M-7dYPNwXfcLzSRGmMTK8mN-?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(4222) Business Analysis Practice":{
+                pastPapers: "https://drive.google.com/drive/folders/18f8iu9LTzCQrqM-7jmJdERq__r67A4Oa?usp=drive_link",
                 markingScheme: "LINK_HERE",
                 shortNotes: "LINK_HERE"
+            },
+            "(4232) Enterprise Architecture":{
+                pastPapers: "https://drive.google.com/drive/folders/1heiurRi7vSP4ZCK0Oc2Fci-9Ayo0exSt?usp=drive_link",
+                markingScheme: "https://drive.google.com/drive/folders/1wY40JIS0rKvaTMRJczkqB-r57KKnHV3n?usp=drive_link",
+                shortNotes: "LINK_HERE"
+            },
+            "(4242) Computer Services Management":{
+                pastPapers: "LINK_HERE",
+                markingScheme: "LINK_HERE",
+                shortNotes: "https://drive.google.com/drive/folders/1W-Cj6-wkD_mFkWKVEDqgj5RBz7Q1Soyk?usp=drive_link"
             }
         }
     }
